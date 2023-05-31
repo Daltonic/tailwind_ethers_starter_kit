526c597ea7afe3b0ce6ff750d02c72033f7c9ecf
Gospel Darlington <darlingtongospel@gmail.com>
2023-05-31 15:05:23 +0000

    upgrades kit to React v18

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <div className="min-h-screen">
-      <h4 className="text-3xl">Hello Truffle and Tailwind</h4>
+      <h4 className="text-3xl">Hello Hardhat and Tailwind</h4>
     </div>
   )
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-)
+  </BrowserRouter>
+);
